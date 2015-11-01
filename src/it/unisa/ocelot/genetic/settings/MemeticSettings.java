212ea7826d000e276f89173ade8138bd98ece825
giograno <granogiovanni90@gmail.com>
2015-11-01 19:07:54 +0100

    Addition of memetic algorithm

---
+++ src/it/unisa/ocelot/genetic/settings/MemeticSettings.java
@@ -0,0 +1,36 @@
+package it.unisa.ocelot.genetic.settings;
+
+
+
+import it.unisa.ocelot.conf.ConfigManager;
+
+import it.unisa.ocelot.genetic.algorithms.MemeticAlgorithm;
+
+import jmetal.core.Algorithm;
+import jmetal.core.Problem;
+import jmetal.util.JMException;
+import jmetal.util.parallel.IParallelEvaluator;
+import jmetal.util.parallel.MultithreadedEvaluator;
+
+public class MemeticSettings extends GASettings {
+
+	public MemeticSettings(Problem pProblem) {
+		super(pProblem);
+	}
+	
+	public MemeticSettings(Problem pProblem, ConfigManager pConfig) {
+		super(pProblem, pConfig);
+	}
+	
+	public Algorithm configure() throws JMException {
+		Algorithm algorithm;
+		
+		IParallelEvaluator parallelEvaluator = new MultithreadedEvaluator(threads);
+		
+		// Creating the problem
+		algorithm = new MemeticAlgorithm(problem_);
+        
+		return configure(algorithm);
+	}
+
+}
