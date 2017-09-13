a9b1937de063d141a61c5cfe813246020ab8841a
Giovanni Grano <granogiovanni90@gmail.com>
2017-09-13 18:04:49 +0200

    Added testSourceDirectory to pom file; dummy test case

---
+++ src/test/TestClass.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    public void dummy() {
+        assertEquals("testing Ocelot", "testing Ocelot");
+    }
+}
